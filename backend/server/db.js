2df870ef20ece172046ea7f24954ba1e85cf7602
Tprog01 <gazalievruslan2@gmail.com>
2026-05-09 16:28:52 +0300

    Золотая осень

---
+++ backend/server/db.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || "golden_fall",
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+module.exports = pool.promise();
